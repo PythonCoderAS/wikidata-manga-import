387bd549d70d025bebf6160a10021f5f66804246
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-12-03 18:46:32 -0500

    Remove romaji since it is not reliable

--- src/providers/anilist.py
+++ src/providers/anilist.py
@@ -4,7 +4,7 @@ from ..data.extra_property import ExtraProperty, ExtraQualifier
 from ..data.smart_precision_time import SmartPrecisionTime
 
 from ..abc.provider import Provider
-from ..constants import Genres, Demographics, site, stated_at_prop, url_prop, mal_id_prop, japan_item, japanese_lang_item, korea_item, korean_lang_item, china_item, chinese_lang_item, country_prop, language_prop, hashtag_prop, anilist_id_prop, official_site_prop, romaji_title_prop, title_prop, anilist_item
+from ..constants import Genres, Demographics, site, stated_at_prop, url_prop, mal_id_prop, japan_item, japanese_lang_item, korea_item, korean_lang_item, china_item, chinese_lang_item, country_prop, language_prop, hashtag_prop, anilist_id_prop, official_site_prop, title_prop, anilist_item
 from ..data.reference import Reference
 from ..data.results import Result
 from ..pywikibot_stub_types import WikidataReference
@@ -166,13 +166,8 @@ class AnilistProvider(Provider):
                         language_claim.setTarget(self.external_links_language_mapping[item["language"]])
                         result.other_properties[official_site_prop][-1].qualifiers[language_prop].append(ExtraQualifier(language_claim))
         if data["title"]:
-            romaji = data["title"]["romaji"]
             english = data["title"]["english"]
             native = data["title"]["native"]
-            if romaji is not None:
-                romaji_claim = pywikibot.Claim(site, romaji_title_prop)
-                romaji_claim.setTarget(romaji.strip())
-                result.other_properties[romaji_title_prop].append(ExtraProperty(claim=romaji_claim, skip_if_conflicting_exists=True))
             if english is not None:
                 english_claim = pywikibot.Claim(site, title_prop)
                 english_claim.setTarget(pywikibot.WbMonolingualText(english.strip(), "en"))
